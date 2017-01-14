49358f88667470372de17562c33dae1d1e1aef22
jverbosk <jverbosk@gmail.com>
2017-01-13 19:30:27 -0500

    updated - mined minds if divisible by 3&5 (15)

--- 2017_01_13.rb
+++ 2017_01_13.rb
@@ -1,11 +1,14 @@
 # Print 1 - 100 to the screen
-# If number is divisible by 3, replace number with "mined"
-# If number is divisible by 5, replace number with "minds"
+# If number is divisible by 3, replace number with "Mined"
+# If number is divisible by 5, replace number with "Minds"
+# If number is divisible by 3 & 5 (15), replace number with "Mined Minds"
 for num in 1..100
-  if num % 3 == 0
-  	puts "mined"
+  if num % 3 == 0 && num % 5 == 0
+    puts "Mined Minds"
+  elsif num % 3 == 0
+  	puts "Mined"
   elsif num % 5 == 0
-  	puts "minds"
+  	puts "Minds"
   else
     puts num
   end
