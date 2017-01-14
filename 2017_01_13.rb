7d8599d130dc6e28f4d03d5d729adef82172d950
jverbosk <jverbosk@gmail.com>
2017-01-13 19:23:59 -0500

    updated - minds if divisible by 5

--- 2017_01_13.rb
+++ 2017_01_13.rb
@@ -1,8 +1,11 @@
 # Print 1 - 100 to the screen
 # If number is divisible by 3, replace number with "mined"
+# If number is divisible by 5, replace number with "minds"
 for num in 1..100
   if num % 3 == 0
   	puts "mined"
+  elsif num % 5 == 0
+  	puts "minds"
   else
     puts num
   end
