ec43b4e82c4d24976c5da81cc8e704522af8fc67
jverbosk <jverbosk@gmail.com>
2017-01-14 16:39:49 -0500

    updated - single-line conditional statement varaition

--- 2017_01_13.rb
+++ 2017_01_13.rb
@@ -3,7 +3,7 @@
 # If number is divisible by 5, replace number with "Minds"
 # If number is divisible by 3 & 5 (15), replace number with "Mined Minds"
 for num in 1..100
-  if num % 3 == 0 && num % 5 == 0
+ if num % 3 == 0 && num % 5 == 0
     puts "Mined Minds"
   elsif num % 3 == 0
     puts "Mined"
@@ -17,3 +17,9 @@ end
 # Single line version using semi-colons to continue
 # Demo-only - not the most readable
 for num in 1..100; if num % 3 == 0 && num % 5 == 0; puts "Mined Minds"; elsif num % 3 == 0; puts "Mined"; elsif num % 5 == 0; puts "Minds"; else puts num; end; end
+
+# Another variation using one line for conditional statement via chained logical operators
+for num in 1..100
+  num = ("Mined Minds" if num % 3 == 0 && num % 5 == 0) || ("Mined" if num % 3 == 0) || ("Minds" if num % 5 == 0) || num
+  puts num
+end
