016d56dd59480cd5a71a1aa58f09f3b35d116893
jverbosk <jverbosk@gmail.com>
2017-01-18 16:53:12 -0500

    array 1-100 and first test

---
+++ 2017_01_18.rb
@@ -0,0 +1,27 @@
+# Need to call ("values") or return ("return values")
+# Using "print values" or "puts values" will error on test
+
+# def minedmindskata()
+#   values = []
+#   (1..100).each { |num| values.push(num) }
+#   values.map! { |value| (value % 3 != 0 || value % 5 != 0) ? value : "Mined Minds" }
+#   values.map! { |value| value % 3 != 0 ? value : "Mined" }
+#   values.map! { |value| value % 5 != 0 ? value : "Minds" }
+#   values
+# end
+
+# puts minedmindskata()
+
+def minedminds()
+	range = []
+	num = 1
+	for item in (1..100)
+		range.push(num)
+        num += 1
+	end
+	return range
+end
+
+print minedminds()
+
+
