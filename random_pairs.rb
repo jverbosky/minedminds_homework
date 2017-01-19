0db11b22dcfd0e8f1d2baa4ee42d772bf6136f7d
jverbosk <jverbosk@gmail.com>
2017-01-19 16:20:29 -0500

    updating for review - tests not working...

--- random_pairs.rb
+++ random_pairs.rb
@@ -17,7 +17,7 @@ names = [
   "Frank Coleman",
   "George Bruner",
   "Jayvon Harris",
-  "John Verbosky",
+#  "John Verbosky", # comment out to test list_pairs_even()
   "Khalifa Cochran",
   "Lee Brown",
   "Lisa Petrovich",
@@ -32,13 +32,16 @@ names = [
 ]
 
 # Function to verify that object being passed in is an array
+# Corresponds to test_1_array_parameter
 def array_test(a)
   if a.class == Array
-    random_sort(a)
+    #puts "Run string_test(a)..."  # basic test
+    string_test(a)
   end
 end
 
 # Function to verify that all array members are strings
+# Corresponds to test_2_string_test
 def string_test(b)
   test = []
   b.each do |item|
@@ -51,30 +54,70 @@ def string_test(b)
   if test.include? false
     return false
   else
+    #puts "Run random_sort(b)..."  # basic test
     random_sort(b)
   end
 end
 
 # Function to randomly scramble items in the array and verify that array members haven't changed
+# Corresponds to test_3_random_sort
 def random_sort(c)
   scrambled = c.shuffle
   if c - scrambled == [] && scrambled - c == []
-  	random_pairs(scrambled)
+  	#puts "Run pair_names(scrambled)..."  # basic test
+  	pair_names(scrambled)
   end
 end
 
-def random_pairs(d)
-  inner_arrays = d.count / 2  # value for number of inner arrays to hold pairs
-  multi = Array.new(inner_arrays) { Array.new(2) }  # create multi-dimensional arrays for each pair
-  if d.count % 2 == 1
-    return true
+# Function to change array into a multi-dimensional array with an inner array for every two names
+# Ex: [[name1,name2],[name3,name4],[name5,name6]]
+# Corresponds to test_4_
+def pair_names(d)
+  paired = d.each_slice(2).to_a
+  #puts "Run evaluate_pairs(paired)..."  # basic test
+  evaluate_pairs(paired)
+end
+
+# Function to determine if the final inner array is paired or solitary
+# If paired call list_pairs_even(), if solitary, call list_pairs_odd()
+# Corresponds to test_5_evaluate_odd
+# and test_6_evaluate_even
+def evaluate_pairs(e)
+  last = e.count - 1
+  if e[last].count == 1
+  	#puts "Run merge_pairs_odd(e)..."  # basic test
+    merge_pairs_odd(e)
   else
-    return false
+  	#puts "Run list_pairs_even(e)..."  # basic test
+    list_pairs_even(e)
   end
-  print multi
 end
 
-puts array_test(names)
-puts string_test(names)
-puts random_sort(names)
-puts random_pairs(names)
+# Function to list array pairs when final inner array has two names
+# Corresponds to test_7_
+def list_pairs_even(pairs)
+  counter = 1
+  pairs.each do |name1, name2| 
+    puts "Random Pair #{counter}: #{name1}, #{name2}"
+    counter += 1
+  end
+end
+
+# Function to combine last two inner arrays when final inner array has one name
+# Final inner array will then have 3 members
+# Corresponds to test_8_
+def merge_pairs_odd(multi)
+  puts "merge last two inner arrays..."
+  list_pairs_odd(multi)
+end
+
+# Function to list array pairs when final inner array has an three names
+# Corresponds to test_9_
+def list_pairs_odd(pairs)
+	puts "output random pairs and final set of three names..."
+end
+
+#array_test(names)
+# puts string_test(names)
+# puts random_sort(names)
+# puts random_pairs(names)
--- random_pairs_test.rb
+++ random_pairs_test.rb
@@ -13,31 +13,55 @@ require_relative "random_pairs.rb"
 # Class to create a new instance for test
 class TestMinedMindsRandomPairs < Minitest::Test
 
+  # # Test 1 - Verify that array_test() will accept an array as a parameter
+  # def test_1_array_parameter
+  # 	mock = MiniTest::Mock.new
+  #   test_array = ["a","b","c","d","e","f"]
+  #   mock.expect(:string_test, nil, ['array_test'])
+  #   @class.stub :string_test, -> (arg) { mock.string_test arg } do
+  #   	@class.array_test
+  #   end
+  #   assert mock.verify
+  # end
+
   # Test 1 - Verify that array_test() will accept an array as a parameter
   def test_1_array_parameter
-    eleven = ["a","b","c","d","e","f","g","h","i","j","k"]
-    assert_equal(true,array_test(eleven))
+    test_array = ["a","b","c","d","e","f"]
+    results = string_test(test_array)
+    #test_array = "test"  # non-string test to verify test fails
+    assert_equal(results,array_test(test_array))
   end
 
-  # Test 2 - Verify that all array members are strings
-  def test_2_string_test
-    eleven = ["a","b","c","d","e","f","g","h","i","j","k"]
-    assert_equal(true,string_test(eleven))
-  end
+  # # Test 2 - Verify that all array members are strings
+  # def test_2_string_test
+  #   test_array = ["a","b","c","d","e","f"]
+  #   #test_array = ["a","b",2]  # non-string member to verify test fails
+  #   assert_equal(random_sort(test_array),string_test(test_array))
+  # end
 
-  # Test 3 - Verify that random_sort() has scrambled the input array and array members haven't changed
-  def test_3_random_sort
-    eleven = ["a","b","c","d","e","f","g","h","i","j","k"]
-    assert_equal(true,random_sort(eleven))
-  end
+  # # Test 3 - Verify that random_sort() has scrambled the input array and array members haven't changed
+  # def test_3_random_sort
+  #   test_array = ["a","b","c","d","e","f"]
+  #   assert_equal(pair_names(test_array),random_sort(test_array))
+  #   #assert_equal(pair_names(test_array.shuffle),random_sort(test_array))
+  # end
 
-  # Test 4 - Verify that a multi-dimensional array is returned by the function
+  # # Test 4 - Verify that a multi-dimensional array with paired inner arrays is returned by the function
+  # def test_3_random_sort
+  #   test_array = ["a","b","c","d","e","f","g","h","i","j","k"]
+  #   test_array_paired = [["a", "b"], ["c", "d"], ["e", "f"]]
+  #   assert_equal(test_array_paired,pair_names(test_array))
+  # end
 
+  # Test 5 - Verify that the merge_pairs_odd() functions is called if final inner array has one name
 
-  # Test 5 - Verify that multi-dimensional items are randomly paired
+  # Test 6 - Verify that the list_pairs_even() functions is called if final inner array has two name
 
+  # Test 7 - Verify that name pairs are printed when all inner arrays have two names
 
-  # Test 6 - Count array items and if odd, verify that one of the 
-  # multi-dimensional array contains an odd number of items
+  # Test 8 - Verify that final inner array contains three names
+
+  # Test 9 - Verify that name pairs are printed when final inner array has three names
+
 
 end
