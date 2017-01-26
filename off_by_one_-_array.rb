34dbbf533c3f977926cebb8316638d1a6caba624
jverbosk <jverbosk@gmail.com>
2017-01-26 17:15:02 -0500

    Bonus homework *truly* cleaned up and done now

--- off_by_one_-_array.rb
+++ off_by_one_-_array.rb
@@ -12,14 +12,17 @@ def compare_number_array(ticket, winning_numbers)
       set_1 = winner.split("")  # convert the current winning number into an array
       set_2 = ticket.split("")  # convert the ticket number into an array
       off_by_one = 0  # counter for winning number digits that are off by one compared to ticket number digit
-      greater_than_one = 0  # counter for winning number digits that are more than one off
+      greater_than_one = 0  # counter for winning number digits off by more than one
       comparison_set = set_1.zip(set_2)  # use .zip method to pair up winning and ticket number digits in multi-d array
       comparison_set.each do |inner|  # iterate through each inner array (pair of digits) to compare
         if (inner[0].to_i - inner[1].to_i).abs == 1  # check if the digits are off by +/- 1
           off_by_one += 1  # if so, increment the off_by_one counter
+        elsif (inner[0].to_i - inner[1].to_i).abs > 1  # check if the digits are off by more than 1
+          greater_than_one += 1  # if so, increment the greater_than_one counter
         end  # compare the next pair of digits
       end
-      if off_by_one == 1  # if a single digit is off by one
+      # if a single digit is off by one and there are no digits off by more than one
+      if off_by_one == 1 && greater_than_one == 0
         winning_numbers_off_by_one_digit.push(winner)  # push the winning number to the appropriate array
       end  # compare the ticket number against the next winning number
     end
@@ -32,19 +35,19 @@ def compare_number_array(ticket, winning_numbers)
 end
 
 # Sandbox testing
-# print compare_number_array('test', ['9999', '5678', '1235', '1134', '1344'])  # false
-# print "\n"
-# print compare_number_array('1234', '1235')  # false
-# print "\n"
-# print compare_number_array('1234', [])  # false
-# print "\n"
-# print compare_number_array('1234', ['9999', '5678', '1235', '1134', '1344'])  # ['1235', '1134']
-# print "\n"
-# print compare_number_array('1234', ['1234', '1235', '1236', '1244', '1334'])  # ['1235', '1244', '1334']
-# print "\n"
-# print compare_number_array('1234', ['2234', '3234', '1335', '1336'])  # ['2234']
-# print "\n"
-# print compare_number_array('1234', ['1111', '2222', '3333'])  # false
-# print "\n"
-# print compare_number_array('123456', ['999999', '567890', '123556', '113456', '134456'])  # ['1235', '1134']
-# print "\n"
+print compare_number_array('test', ['9999', '5678', '1235', '1134', '1344'])  # false
+print "\n"
+print compare_number_array('1234', '1235')  # false
+print "\n"
+print compare_number_array('1234', [])  # false
+print "\n"
+print compare_number_array('1234', ['9999', '5678', '1235', '1134', '1344'])  # ['1235', '1134']
+print "\n"
+print compare_number_array('1234', ['1234', '1235', '1236', '1244', '1334'])  # ['1235', '1244', '1334']
+print "\n"
+print compare_number_array('1234', ['2234', '3234', '1335', '1336'])  # ['2234']
+print "\n"
+print compare_number_array('1234', ['1111', '2222', '3333'])  # false
+print "\n"
+print compare_number_array('123456', ['999999', '567890', '123556', '113456', '134456'])  # ['123556', '113456']
+print "\n"
--- off_by_one_-_array_-_explained.rb
+++ off_by_one_-_array_-_explained.rb
@@ -2,13 +2,13 @@
 
 def compare_number_array(ticket, winning_numbers)
   winning_numbers_off_by_one_digit = []
-  winning_numbers_no_match = []
-  same_number = 0
+  # winning_numbers_no_match = []
+  # same_number = 0
   count = 0
   winning_numbers.each do |winner|
-    if winner == ticket
-      same_number += 1
-    else
+    # if winner == ticket
+    #   same_number += 1
+    # else
       set_1 = winner.split("")
       set_2 = ticket.split("")
       puts "Iteration count: #{count}"
@@ -24,30 +24,27 @@ def compare_number_array(ticket, winning_numbers)
       print comparison_set
       print "\n"
       comparison_set.each do |inner|
-        if inner[0] != inner[1]
+        # if inner[0] != inner[1]
+          puts (inner[0].to_i - inner[1].to_i).abs
           if (inner[0].to_i - inner[1].to_i).abs == 1
             off_by_one += 1
           elsif (inner[0].to_i - inner[1].to_i).abs > 1
             greater_than_one += 1
           end
-        end
+        # end
       end
       puts "Off by one count: #{off_by_one}"
       puts "Greater than one count: #{greater_than_one}"
       puts "Winning ticket: #{winner}"
       print "\n"
-      if greater_than_one > 0
-        winning_numbers_no_match.push(winner)
-      elsif off_by_one > 1
-        winning_numbers_no_match.push(winner)
-      elsif off_by_one == 1
+      if off_by_one == 1 && greater_than_one == 0
         winning_numbers_off_by_one_digit.push(winner)
       end
-    end
+    # end
   end
-  puts "Same number count: #{same_number}"
-  print "Winning numbers no match: #{winning_numbers_no_match}"
-  print "\n"
+ # puts "Same number count: #{same_number}"
+ # print "Winning numbers no match: #{winning_numbers_no_match}"
+ # print "\n"
   print "Winning numbers off by one digit: #{winning_numbers_off_by_one_digit}"
   print "\n"
 end
@@ -57,14 +54,14 @@ end
 # print "\n"
 # print compare_number_array('1234', '1235')  # false
 # print "\n"
-print compare_number_array('1234', [])  # false
-print "\n"
+# print compare_number_array('1234', [])  # false
+# print "\n"
 # print compare_number_array('1234', ['9999', '5678', '1235', '1134', '1344'])  # ['1235', '1134']
 # print "\n"
 # print compare_number_array('1234', ['1234', '1235', '1236', '1244', '1334'])  # ['1235', '1244', '1334']
 # print "\n"
-# print compare_number_array('1234', ['2234', '3234', '1335', '1336'])  # ['2234']
-# print "\n"
+print compare_number_array('1234', ['2234', '3234', '1335', '1336'])  # ['2234']
+print "\n"
 # print compare_number_array('1234', ['1111', '2222', '3333'])  # false
 # print "\n"
 # print compare_number_array('123456', ['999999', '567890', '123556', '113456', '134456'])  # ['1235', '1134']
