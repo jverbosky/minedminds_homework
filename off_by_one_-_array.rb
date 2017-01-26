dc91972c20a4fc4909e48fbd8099d135ad7883d9
jverbosk <jverbosk@gmail.com>
2017-01-26 15:18:06 -0500

    Function cleaned up, sandbox tests working, first test working

--- off_by_one_-_array.rb
+++ off_by_one_-_array.rb
@@ -6,26 +6,15 @@ def compare_number_array(ticket, winning_numbers)
   winning_numbers_off_by_one_digit = []
   winning_numbers_no_match = []
   same_number = 0
-  count = 0
   winning_numbers.each do |winner|
     if winner == ticket
       same_number += 1
     else
       set_1 = winner.split("")
       set_2 = ticket.split("")
-      puts "Iteration count: #{count}"
-      print "Set 1: #{set_1}"
-      print "\n"
-      print "Set 2: #{set_2}"
-      print "\n"
-      count += 1
-
       off_by_one = 0
       greater_than_one = 0
       comparison_set = set_1.zip(set_2)
-      puts "Comparison set:"
-      print comparison_set
-      print "\n"
       comparison_set.each do |inner|
         if inner[0] != inner[1]
           if (inner[0].to_i - inner[1].to_i).abs == 1
@@ -35,9 +24,6 @@ def compare_number_array(ticket, winning_numbers)
           end
         end
       end
-      puts "Off by one count: #{off_by_one}"
-      puts "Greater than one count: #{greater_than_one}"
-      puts "Winning ticket: #{winner}"
       if greater_than_one > 0
         winning_numbers_no_match.push(winner)
       elsif off_by_one > 1
@@ -47,15 +33,15 @@ def compare_number_array(ticket, winning_numbers)
       end
     end
   end
-  puts "Same number count: #{same_number}"
-  print "Winning numbers no match: #{winning_numbers_no_match}"
-  print "\n"
-  print "Winning numbers off by one digit: #{winning_numbers_off_by_one_digit}"
+  return winning_numbers_off_by_one_digit
 end
 
 # Sandbox testing
-puts compare_number_array('1234', ['9999', '5678', '1235', '1134', '1344'])  # ['1235', '1134']
-# puts compare_number_array('1234', ['1234', '1235', '1236', '1244', '1334'])  # ['1235', '1244', '1334']
-# puts compare_number_array('1234', ['2234', '3234', '1335', '1336'])  # ['2234']
-# puts compare_number_array('1234', ['1111', '2222', '3333'])  # []
+# print compare_number_array('1234', ['9999', '5678', '1235', '1134', '1344'])  # ['1235', '1134']
+# print "\n"
+# print compare_number_array('1234', ['1234', '1235', '1236', '1244', '1334'])  # ['1235', '1244', '1334']
+# print "\n"
+# print compare_number_array('1234', ['2234', '3234', '1335', '1336'])  # ['2234']
+# print "\n"
+# print compare_number_array('1234', ['1111', '2222', '3333'])  # []
 
