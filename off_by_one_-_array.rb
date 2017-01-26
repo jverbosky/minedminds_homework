a6e39b60c7008375f419e29e6bf0e82ef305329b
jverbosk <jverbosk@gmail.com>
2017-01-26 15:39:18 -0500

    Extra Bonus Homework completed - tweaked top comment for function to explain purpose

--- off_by_one_-_array.rb
+++ off_by_one_-_array.rb
@@ -1,6 +1,6 @@
-# Function that takes two numbers as strings and returns true if they are off by one digit
-# Use case 1: If we pass '1234' and '1224' (1 digit off in tens place), the function should return true
-# Use case 2: If we pass '1234' and '1325' (more than 1 digit off), the function should return false
+# Function that takes a ticket number and an array of winning numbers
+# Returns an array of all of the winning numbers that are one digit off from the ticket number
+# If no winning numbers match this condition, returns false
 
 def compare_number_array(ticket, winning_numbers)
   winning_numbers_off_by_one_digit = []
