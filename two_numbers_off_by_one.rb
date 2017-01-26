e28ec4aa734406e5a9dc79d8595fcd53e92daa5d
jverbosk <jverbosk@gmail.com>
2017-01-26 11:07:44 -0500

    first idea - not quite what I was looking for, will research...

--- two_numbers_off_by_one.rb
+++ two_numbers_off_by_one.rb
@@ -2,3 +2,23 @@
 # Use case 1: If we pass '1234' and '1224' (1 digit off in tens place), the function should return true
 # Use case 2: If we pass '1234' and '1325' (more than 1 digit off), the function should return false
 
+number_1 = '1234'
+number_2 = '1224'
+number_3 = '1325'
+
+def compare_numbers(num_1, num_2)
+  set_1 = num_1.split("")
+  print "\n"
+  puts "Beginning comparison..."
+  print set_1
+  print "\n"
+  set_2 = num_2.split("")
+  print set_2
+  print "\n"
+  compare = set_1 & set_2
+  print compare
+  print "\n"
+end
+
+compare_numbers(number_1, number_2)
+compare_numbers(number_1, number_3)
