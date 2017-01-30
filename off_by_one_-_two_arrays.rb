a64f7c00b76f59978652e290ef69a145e83f0a5a
jverbosk <jverbosk@gmail.com>
2017-01-30 12:35:20 -0500

    Working on third function - returning some but not all expected unclaimed winning tickets, reviewing final if statement logic

--- off_by_one_-_two_arrays.rb
+++ off_by_one_-_two_arrays.rb
@@ -1,16 +1,50 @@
-# Function that takes an array of ticket numbers and an array of winning numbers
+# Functions that takes an array of ticket numbers and an array of winning numbers
 # 1) Returns an array of ticket numbers that exactly match the winning numbers
 # 2) Returns an array of all of the winning numbers that are one digit off from the ticket number
 # 3) Returns an array of all of the winning numbers that do not meet the first two conditions
 
-def find_winning_ticket_numbers(ticket_numbers, winning_numbers)
-  winning_numbers_match = []  # array for winning numbers that match ticket numbers
-  winning_numbers_match = ticket_numbers & winning_numbers  # array intersection for finding matching numbers
-  return winning_numbers_match  # return array of winning numbers that match ticket numbers
-end
+# def find_winning_ticket_numbers(ticket_numbers, winning_numbers)
+#   winning_numbers_match = []  # array for winning numbers that match ticket numbers
+#   winning_numbers_match = ticket_numbers & winning_numbers  # array intersection for finding matching numbers
+#   return winning_numbers_match  # return array of winning numbers that match ticket numbers
+# end
 
-def find_ticket_numbers_off_by_one(ticket_numbers, winning_numbers)
-  ticket_numbers_off_by_one_digit = []  # array for ticket numbers to return
+# def find_ticket_numbers_off_by_one(ticket_numbers, winning_numbers)
+#   ticket_numbers_off_by_one_digit = []  # array for ticket numbers to return
+
+#   ticket_numbers.each do |ticket|  # iterate through each ticket number in the array
+
+#     winning_numbers.each do |winner|  # iterate through each winning number in the array
+#       set_1 = winner.split("")  # convert the current winning number into an array of digits
+#       set_2 = ticket.split("")  # convert the ticket number into an array of digits
+#       off_by_one = 0  # counter for winning number digits that are off by one compared to ticket number digit
+#       greater_than_one = 0  # counter for winning number digits off by more than one
+#       comparison_set = set_1.zip(set_2)  # use .zip method to pair up winning and ticket number digits in multi-d array
+
+#       comparison_set.each do |inner|  # iterate through each inner array (pair of digits) to compare
+#         if inner[0] != inner[1]  # check if the digits are identical and if they are not...
+#           if (inner[0].to_i - inner[1].to_i).abs == 1  # check if the digits are off by +/- 1
+#             off_by_one += 1  # if so, increment the off_by_one counter
+#           elsif (inner[0].to_i - inner[1].to_i).abs > 1  # check if the digits are off by more than 1
+#             greater_than_one += 1  # if so, increment the greater_than_one counter
+#           end  # end the conditional counter increment statements
+#         end  # restart the loop and compare the next pair of digits
+#       end  # end the comparison_set.each loop
+
+#       # if a single digit is off by one and there are no digits off by more than one
+#       if off_by_one == 1 && greater_than_one == 0
+#         ticket_numbers_off_by_one_digit.push(ticket)  # push the ticket number to the appropriate array
+#       end  # start the next winning_numbers iteration to compare the ticket number against the next winning number
+
+#     end  # start the next ticket_numbers iteration to compare the next ticket number against the winning numbers
+#   end  # end the ticket_numbers.each loop
+
+#   return ticket_numbers_off_by_one_digit  # return list of ticket numbers off by one for a single digit
+
+# end
+
+def find_unclaimed_winning_numbers(ticket_numbers, winning_numbers)
+  unclaimed_winning_numbers = []  # array for winning numbers to return
 
   ticket_numbers.each do |ticket|  # iterate through each ticket number in the array
 
@@ -32,71 +66,22 @@ def find_ticket_numbers_off_by_one(ticket_numbers, winning_numbers)
       end  # end the comparison_set.each loop
 
       # if a single digit is off by one and there are no digits off by more than one
-      if off_by_one == 1 && greater_than_one == 0
-        ticket_numbers_off_by_one_digit.push(ticket)  # push the ticket number to the appropriate array
+      if off_by_one > 1 && greater_than_one > 0
+        unclaimed_winning_numbers.push(winner)  # push the ticket number to the appropriate array
       end  # start the next winning_numbers iteration to compare the ticket number against the next winning number
 
     end  # start the next ticket_numbers iteration to compare the next ticket number against the winning numbers
   end  # end the ticket_numbers.each loop
 
-  return ticket_numbers_off_by_one_digit  # return list of ticket numbers off by one for a single digit
+  return unclaimed_winning_numbers  # return list of ticket numbers off by one for a single digit
 
 end
 
-# Sandbox testing
-# find_winning_ticket_numbers(['1234', '5678'], ['6678', '1235', '5679', '1134', '1234', '1344'])
-print find_ticket_numbers_off_by_one(['1225', '1234', '5678', '6679'], ['6778', '2225', '6779', '1144', '1234', '1344'])
 
-# For reference:
-#
-# def compare_number_array(ticket, winning_numbers)
-#   # return false if arguments passed in are the wrong type
-#   if !/\A\d+\z/.match(ticket) || winning_numbers.class != Array || winning_numbers == []
-#     return false
-#   else
-#     winning_numbers_off_by_one_digit = []  # array for numbers to return
-#     winning_numbers.each do |winner|  # iterate through each winning number
-#       set_1 = winner.split("")  # convert the current winning number into an array
-#       set_2 = ticket.split("")  # convert the ticket number into an array
-#       off_by_one = 0  # counter for winning number digits that are off by one compared to ticket number digit
-#       greater_than_one = 0  # counter for winning number digits off by more than one
-#       comparison_set = set_1.zip(set_2)  # use .zip method to pair up winning and ticket number digits in multi-d array
-#       comparison_set.each do |inner|  # iterate through each inner array (pair of digits) to compare
-#         if inner[0] != inner[1]  # check if the digits are identical and if they are not...
-#           if (inner[0].to_i - inner[1].to_i).abs == 1  # check if the digits are off by +/- 1
-#             off_by_one += 1  # if so, increment the off_by_one counter
-#           elsif (inner[0].to_i - inner[1].to_i).abs > 1  # check if the digits are off by more than 1
-#             greater_than_one += 1  # if so, increment the greater_than_one counter
-#           end
-#         end  # restart the loop and compare the next pair of digits
-#       end
-#       # if a single digit is off by one and there are no digits off by more than one
-#       if off_by_one == 1 && greater_than_one == 0
-#         winning_numbers_off_by_one_digit.push(winner)  # push the winning number to the appropriate array
-#       end  # compare the ticket number against the next winning number
-#     end
-#     if winning_numbers_off_by_one_digit.length > 0  # if there are winning numbers in the appropriate array
-#       return winning_numbers_off_by_one_digit  # return the array of desired winning numbers
-#     else  # or else there are none
-#       return false  # so return false
-#     end
-#   end
-# end
 
 # Sandbox testing
-# print compare_number_array('test', ['9999', '5678', '1235', '1134', '1344'])  # false
+# print find_winning_ticket_numbers(['1234', '5678'], ['6678', '1235', '5679', '1134', '1234', '1344'])
 # print "\n"
-# print compare_number_array('1234', '1235')  # false
+# print find_ticket_numbers_off_by_one(['1225', '1234', '5678', '6679'], ['6778', '2225', '6779', '1144', '1234', '1344'])
 # print "\n"
-# print compare_number_array('1234', [])  # false
-# print "\n"
-# print compare_number_array('1234', ['9999', '5678', '1235', '1134', '1344'])  # ['1235', '1134']
-# print "\n"
-# print compare_number_array('1234', ['1234', '1235', '1236', '1244', '1334'])  # ['1235', '1244', '1334']
-# print "\n"
-# print compare_number_array('1234', ['2234', '3234', '1335', '1336'])  # ['2234']
-# print "\n"
-# print compare_number_array('1234', ['1111', '2222', '3333'])  # false
-# print "\n"
-# print compare_number_array('123456', ['999999', '567890', '123556', '113456', '134456'])  # ['123556', '113456']
-# print "\n"
+print find_unclaimed_winning_numbers(['1225', '1234', '5678', '6679'], ['6778', '2225', '6779', '1144', '1234', '1344'])
